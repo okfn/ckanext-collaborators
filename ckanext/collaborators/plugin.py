6a86e34e3724ceede5cdf2c70440230fb0596d16
pdelboca <patriciodelboca@gmail.com>
2019-05-29 11:29:17 -0300

    [IVT-1003] Add blueprints and helpers to plugin.py

--- ckanext/collaborators/plugin.py
+++ ckanext/collaborators/plugin.py
@@ -3,8 +3,13 @@ import logging
 import ckan.plugins as p
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.collaborators.helpers import get_collaborators
 from ckanext.collaborators.model import tables_exist
 from ckanext.collaborators.logic import action, auth
+
+from ckanext.collaborators.views.package import collaborators, collaborator_delete, CollaboratorEditView
+
+from flask import Blueprint
 
 log = logging.getLogger(__name__)
 
@@ -13,6 +18,8 @@ class CollaboratorsPlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
     p.implements(p.IActions)
     p.implements(p.IAuthFunctions)
+    p.implements(p.IBlueprint)
+    p.implements(p.ITemplateHelpers)
 
     # IConfigurer
 
@@ -45,3 +52,26 @@ to create the database tables:
             'dataset_collaborator_delete': auth.dataset_collaborator_delete,
             'dataset_collaborator_list': auth.dataset_collaborator_list,
         }
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {'collaborators_get_collaborators': get_collaborators}
+
+    # IBlueprint
+    def get_blueprint(self):
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+
+        blueprint.add_url_rule(rule=u'/datasets/collaborators/<dataset_id>',
+                                endpoint='read',
+                                view_func=collaborators, methods=['GET',])
+        
+        blueprint.add_url_rule(rule=u'/datasets/collaborators/<dataset_id>/new', 
+                                view_func=CollaboratorEditView.as_view('new'), methods=['GET', 'POST',])
+
+        blueprint.add_url_rule(rule=u'/datasets/collaborators/<dataset_id>/delete/<user_id>',
+                                endpoint='delete',
+                                view_func=collaborator_delete, methods=['POST',])
+
+        return blueprint
+
