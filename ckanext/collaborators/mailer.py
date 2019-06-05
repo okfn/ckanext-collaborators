d2900157464a56cde8d293e495794e32aa84c5b5
pdelboca <patriciodelboca@gmail.com>
2019-06-05 16:35:20 -0300

    [IVT-1004] Add mailer.py module

---
+++ ckanext/collaborators/mailer.py
@@ -0,0 +1,32 @@
+import logging
+from ckan import model as core_model
+from ckan.plugins import toolkit
+from ckan.lib.mailer import mail_user, MailerException
+from ckan.lib.base import render_jinja2
+log = logging.getLogger(__name__)
+
+
+def _compose_email_subj(dataset):
+    return '{0} - Notification about collaborator role for {1}'.format(
+        toolkit.config.get('ckan.site_title'), dataset.title)
+
+def _compose_email_body(user, dataset, role, event):
+    return render_jinja2('emails/{0}_collaborator.txt'.format(event), {
+        'user_name': user.fullname or user.name,
+        'role': role,
+        'site_title': toolkit.config.get('ckan.site_title'),
+        'site_url': toolkit.config.get('ckan.site_url'),
+        'dataset_title': dataset.title,
+        'dataset_link': dataset.as_dict()['ckan_url']
+})
+
+def mail_notification_to_collaborator(dataset_id, user_id, capacity, event):
+    user = core_model.User.get(user_id)
+    dataset = core_model.Package.get(dataset_id)
+
+    try:
+        subj = _compose_email_subj(dataset)
+        body = _compose_email_body(user, dataset, capacity, event)
+        mail_user(user, subj, body, headers={})
+    except MailerException as exception:
+        log.exception(exception)
